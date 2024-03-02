f047f7bbf3b635ff7355f8328db940ffc942741b
Rishi Viswanathan <rishi.aangarai@gmail.com>
2024-03-02 19:33:27 +0530

    feat: bootstrap app

---
+++ client/app/routes/_index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="w-screen h-screen gap-5 flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Discordia</h1>
+      <p className="text-xl">Welcome to the home page!</p>
+    </div>  
+  );
+}
+
+export default Home;
